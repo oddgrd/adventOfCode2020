8028748803b56651efaa7b86b9af86537db4313c
oddgrd <oddgrd@gmail.com>
2020-12-31 17:01:39 +0100

    works
    Correct result on both, but needs work

--- adventofcode/eigth/eigth.js
+++ adventofcode/eigth/eigth.js
@@ -6,10 +6,8 @@ class Instruction {
     constructor(key, value) {
         this.key = key;
         this.value = value;
-        this.count = 0;
-        this.flipped = false;
     };
-    flipSwitch() {
+    flipKey() {
         this.key === "jmp" ? this.key = "nop" : this.key = "jmp";
     };
 };
@@ -24,24 +22,43 @@ function parseInstruction(str) {
 
 function runGame(instructions) {
     let accumulator = 0;
+    let visitedIdx = [];
     for (let i = 0; i < instructions.length; i++) {
-        if (instructions[i].count === 1) {
-            return accumulator; 
-        } else {   
+        if (visitedIdx.includes(i)) {
+            //For part one I returned accumulator here, for part two
+            //I changed it to return "Infinite"
+            return "Infinite"; 
+        } else {
             switch (instructions[i].key) {
                 case "acc":
-                    instructions[i].count++;
+                    visitedIdx.push(i);
                     accumulator += instructions[i].value;
                     break;
                 case "jmp":
-                    instructions[i].count++;
+                    visitedIdx.push(i);
                     i += instructions[i].value - 1;
+                    break;
                 case "nop":
-                    instructions[i].count++;
+                    visitedIdx.push(i);
                     break;
             }
         }
     }
+    return accumulator;
+};
+
+function runGameTwo(instructions) {
+    for (let i = 0; i < instructions.length; i++) {
+        if (instructions[i].key === "jmp" || instructions[i].key === "nop") {
+            instructions[i].flipKey();
+            var result = runGame(instructions);
+            instructions[i].flipKey();
+            if (result !== "Infinite") {
+                return result;
+            }
+        }
+    }
+    return "All infinite";
 };
 
 function main() {
@@ -49,8 +66,7 @@ function main() {
     const split = data.split(/\n/);
     const instructions = split.filter(x => x !== "").map(x => parseInstruction(x));
 
-    return `Part one: ${runGame(instructions)}`;
+    return `Part one: ${runGame(instructions)} Part two: ${runGameTwo(instructions)}`;
 };
 
 console.log(main());
-
