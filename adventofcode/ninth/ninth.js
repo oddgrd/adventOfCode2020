2f9e75a3a27bab885ee3b0c43f95e0abaa20478b
oddgrd <oddgrd@gmail.com>
2021-01-01 20:57:19 +0100

    part one cleaned

--- adventofcode/ninth/ninth.js
+++ adventofcode/ninth/ninth.js
@@ -2,36 +2,25 @@
 
 const fs = require("fs");
 
-function parsePreamble(numbers, idx) {
-    const preamble = numbers.slice(idx, 25 + idx);
-    return preamble;
-};
-
-function sumBool(nr1, nr2, target) {
-    return nr1 + nr2 === target;
-};
-
 function searchPreamble(preamble, target) {
     const preambleReversed = [...preamble].reverse();
     for (let i = 0; i < preamble.length; i++) {
-        for (let y = 0; y < preambleReversed.length; y++) {
-            if (sumBool(preamble[i], preambleReversed[y], target)) {
+        for (let j = 0; j < preambleReversed.length; j++) {
+            if (preamble[i] + preambleReversed[j] === target) {
                 return false;
             };
         };
     };
-    return target;
+    return true;
 };
 
 function findInvalid(numbers) {
-    let y = 0;
+    let j = 0;
     for (let i = 25; i < numbers.length; i++) {
-        let preamble = parsePreamble(numbers, y);
-        y++;
-        let result = searchPreamble(preamble, numbers[i]);
-        
-        if (result) {
-            return result;
+        let preamble = numbers.slice(j, 25 + j);
+        j++;
+        if (searchPreamble(preamble, numbers[i])) {
+            return number[i];
         };
     };
     return "No invalid";
@@ -42,12 +31,12 @@ function findContiguous(numbers, target) {
     for (let i = 25; i < numbers.length; i++) {
         contiguous.push(numbers[i]);
         let sum = numbers[i];
-        let y = 1;
+        let j = 1;
 
         while (sum < target) {
-            contiguous.push(numbers[i + y]);
-            sum += numbers[i + y];
-            y++;
+            contiguous.push(numbers[i + j]);
+            sum += numbers[i + j];
+            j++;
         };
         if (sum === target) {
             return Math.min(...contiguous) + Math.max(...contiguous);
