aa0d660fcf236d0ed148d370087d20c721de71e2
Oddgrd <oddgrd@gmail.com>
2021-01-01 17:03:19 +0100

    move main loop to function findInvalid

--- adventofcode/ninth/ninth.js
+++ adventofcode/ninth/ninth.js
@@ -11,7 +11,7 @@ function sumBool(nr1, nr2, target) {
     return nr1 + nr2 === target;
 };
 
-function findWeakness(preamble, target) {
+function searchPreamble(preamble, target) {
     const preambleReversed = [...preamble].reverse();
     for (let i = 0; i < preamble.length; i++) {
         for (let y = 0; y < preambleReversed.length; y++) {
@@ -23,25 +23,30 @@ function findWeakness(preamble, target) {
     return target;
 };
 
+function findInvalid(numbers) {
+    let y = 0;
+    for (let i = 25; i < numbers.length; i++) {
+        let preamble = parsePreamble(numbers, y);
+        y++;
+        let result = searchPreamble(preamble, numbers[i]);
+        
+        if (result) {
+            return result;
+        };
+    };
+    return "No invalid";
+};
+
+function findContiguos(numbers, target) {
+    let contiguos = [];
+};
+
 function main(){
     const data = fs.readFileSync("ninthdata.txt", "utf-8");
     const split = data.split(/\n/);
     const numbers = split.filter(x => x !== "").map(x => parseInt(x));
     
-    let weakness;
-    let y = 0;
-    for (let i = 25; i < numbers.length; i++) {
-        let preamble = parsePreamble(numbers, y);
-        y++;
-        let result = findWeakness(preamble, numbers[i]);
-        
-        if (result) {
-            weakness = result;
-            break;
-        };
-    };
-
-    return `First weakness in the cipher: ${weakness}`;
+    return `First invalid number: ${findInvalid(numbers)}`;
 };
 
 console.log(main());
